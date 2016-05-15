5f4e6e6c4574f59016ddf9363e65a78dd69c1e16
zhoouhai02 <zhouhai02@meituan.com>
2016-05-15 16:35:25 +0800

    delete java-server

--- java_server/src/main/java/com/changyu/foryou/service/KeyService.java
+++
@@ -1,9 +0,0 @@
-package com.changyu.foryou.service;
-
-import java.util.Map;
-
-public interface KeyService {
-
-	String SelectKey(Map<String, Object> paramMap);
-
-}
--- java_server/src/main/java/com/changyu/foryou/service/PushService.java
+++
@@ -1,7 +0,0 @@
-package com.changyu.foryou.service;
-
-public interface  PushService {
-	public  void sendPush(String phone, String message, Integer count);
-
-	public void sendPushByTag(String tag, String string, int j);
-}
